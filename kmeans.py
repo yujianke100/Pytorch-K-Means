1dfde729c0c28d0db3829377f7196f0726795bd1
Shinshi <jiankey.zjgsu@gmail.com>
2020-12-16 21:08:38 +0800

    Add files via upload

--- kmeans.py
+++ kmeans.py
@@ -2,11 +2,14 @@ import numpy as np
 import time
 from matplotlib import pyplot as plt
 
-BAR_LEN = 0.05
 loop_time = 0
+K_MIN = 2
+K_MAX = 8
+POINT_MIN = 50
+POINT_MAX = 500
+MAX_LOOP = 100
 
-
-def k_means(data, k, max_time=100):
+def k_means(data, k, max_time=MAX_LOOP):
     global loop_time
     data_size, rgb = data.shape
     k_points = data[np.random.randint(0, data_size, k)]
@@ -29,21 +32,24 @@ def k_means(data, k, max_time=100):
     return last_labels.astype(np.float64)
 
 
-def get_k_means(k):
+def get_k_means(k, point_num):
     global loop_time
     start = time.time()
-    color_box = ['y', 'm', 'c', 'r', 'g', 'b', 'w', 'k']
+    if(k <= 8):
+        color_box = ['y', 'm', 'c', 'r', 'g', 'b', 'w', 'k']
+    else:
+        color_box = range(k)
     for i in range(k):
-        tmp_data = np.random.randn(100, 2) + i
+        tmp_data = np.random.randn(point_num, 2) + i
         if(i == 0):
             data = tmp_data
         else:
             data = np.concatenate([data, tmp_data], 0)
-    data.reshape(k, 100, 2)
+    data.reshape(k, point_num, 2)
     color = k_means(data, k).tolist()
     color = [(lambda x: color_box[int(x)])(i) for i in color]
     plt.figure('result')
-    plt.scatter(data[:, 0], data[:, 1], color=color)
+    plt.scatter(data[:, 0], data[:, 1], c=color)
     used_time = time.time() - start
     plt.title('result k={} in {:.3f}s with {} loop'.format(
         k, used_time, loop_time))
@@ -51,13 +57,29 @@ def get_k_means(k):
 
 
 if __name__ == '__main__':
+    point_num = 100
     print('k means 分类随机点')
     while(1):
-        k = input('请输入k的大小（2-8），或输入q退出：')
-        if(k == 'q'):
-            break
-        elif(k >= '2' and k <= '8'):
-            get_k_means(int(k))
+        k = input('请输入k的大小（{}-{}），或输入q退出：'.format(K_MIN, K_MAX))
+        try:
+            if(k == 'q'):
+                break
+            k = int(k)
+        except:
+            print('只允许输入数字和q，请重试。')
+            continue
+        if(k >= K_MIN and k <= K_MAX):
+            while(1):
+                point_num = input('请输入每组的点数({}-{})：'.format(POINT_MIN, POINT_MAX))
+                try:
+                    point_num = int(point_num)
+                    if(point_num < POINT_MIN or point_num > POINT_MAX):
+                        print('输入错误，只允许输入{}到{}的数字，请重新输入。'.format(POINT_MIN, POINT_MAX))
+                        continue
+                    break
+                except:
+                    print('输入错误，只允许输入{}到{}的数字，请重新输入。'.format(POINT_MIN, POINT_MAX))
+            get_k_means(int(k), point_num)
         else:
             print('输入异常，请重新输入。')
     print('Bye')
